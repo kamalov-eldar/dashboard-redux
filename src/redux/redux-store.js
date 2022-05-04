ee2ec249a0ada794310210d6663d90e4cb308df1
kamalov-eldar <79894966+kamalov-eldar@users.noreply.github.com>
2022-05-04 20:00:42 +0300

    Update redux-store.js

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -11,7 +11,7 @@ let reducers = combineReducers({
 
 let store = createStore(
   reducers,
-  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(thunk)),
 );
 window.store = store;
 
